9beaa6ea5c92b4b883004b24f6df1378b494448c
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-11-16 22:00:07 +0900

    マイグレーションの設定

--- db/migrate/20201116125216_create_posts.rb
+++ db/migrate/20201116125216_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.text :image,      null: false
+      t.string :subject,  null: false
+      t.text :text,       null: false
+      t.integer :user_id, foreign_key: true
       t.timestamps
     end
   end
