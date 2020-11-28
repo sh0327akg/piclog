cae27bd04c4fbe494e70259cbf35eead388d7a0d
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-11-28 21:25:58 +0900

    修正

--- spec/models/devise_spec.rb
+++ spec/models/devise_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nicknameがない場合は登録できないこと" do
-      user = User.new(nickname: nil,email: )
+      user = User.new(nickname: nil,email: "aaa@a.com",password: "00000000",password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
     end
   end
 end
